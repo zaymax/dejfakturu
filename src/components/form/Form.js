4da435e3dd02274294307049c7a9be9b52197c81
Maksim Zaytsev <m.zaytsev@protank.cz>
2022-02-11 22:48:37 +0100

    add handlers to other components

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -6,19 +6,19 @@ import { Section } from "../section/Section";
 import "./form.scss";
 
 export class Form extends Component {
-  today = new Date();
 
   constructor(props) {
     super(props);
     this.state = { 
+
       //Basic information of invoice
       invoiceNumber: null,
       registrationNumber: null,
       variableSymbol: "",
       constantSymbol: "",
       createdBy: "",
-      issueDate:  new Date(Date.UTC(this.today.getMonth(), this.today.getDay(), this.today.getFullYear())),
-      dueDate: new Date(),
+      issueDate:  null,
+      dueDate: null,
       accountNumber: "",
 
       //Supplier
@@ -43,12 +43,31 @@ export class Form extends Component {
 
       //Note
       note: "",
+
     };
 
     this.handleOnChangeInvoiceNumber = this.handleOnChangeInvoiceNumber.bind(this);
+    this.handleOnChangeRegistrationNumber = this.handleOnChangeRegistrationNumber.bind(this);
+    this.handleOnChangeVariableSymbol = this.handleOnChangeVariableSymbol.bind(this);
     this.handleOnChangeConstantSymbol = this.handleOnChangeConstantSymbol.bind(this);
     this.handleOnChangeCreatedBy = this.handleOnChangeCreatedBy.bind(this);
+    this.handleOnChangeIssueDate = this.handleOnChangeIssueDate.bind(this);
+    this.handleOnChangeDueDate = this.handleOnChangeDueDate.bind(this);
+    this.handleOnChangeAccountNumber = this.handleOnChangeAccountNumber.bind(this);
 
+    this.handleOnChangeIdentifierNumberOfSupplier = this.handleOnChangeIdentifierNumberOfSupplier.bind(this);
+    this.handleOnChangeNameOfSupplier = this.handleOnChangeNameOfSupplier.bind(this);
+    this.handleOnChangeVatIdentifierNumberOfSupplier = this.handleOnChangeVatIdentifierNumberOfSupplier.bind(this);
+    this.handleOnChangeStreetOfSupplier = this.handleOnChangeStreetOfSupplier.bind(this);
+    this.handleOnChangeCityOfSupplier = this.handleOnChangeCityOfSupplier.bind(this);
+    this.handleOnChangeZipCodeOfSupplier = this.handleOnChangeZipCodeOfSupplier.bind(this);
+
+    this.handleOnChangeIdentifierNumberOfClient = this.handleOnChangeIdentifierNumberOfClient.bind(this);
+    this.handleOnChangeNameOfClient = this.handleOnChangeNameOfClient.bind(this);
+    this.handleOnChangeVatIdentifierNumberOfClient = this.handleOnChangeVatIdentifierNumberOfClient.bind(this);
+    this.handleOnChangeStreetOfClient = this.handleOnChangeStreetOfClient.bind(this);
+    this.handleOnChangeCityOfClient = this.handleOnChangeCityOfClient.bind(this);
+    this.handleOnChangeZipCodeOfClient = this.handleOnChangeZipCodeOfClient.bind(this);
   }
 
   handleOnChangeInvoiceNumber(event) {
@@ -72,20 +91,68 @@ export class Form extends Component {
   }
 
   handleOnChangeIssueDate(event) {
-    this.setState({issueDate: event.target.value})
+    this.setState({issueDate: event.target.value});
   }
 
+  handleOnChangeDueDate(event) {
+    this.setState({dueDate: event.target.value});
+  }
+
+  handleOnChangeAccountNumber(event) {
+    this.setState({accountNumber: event.target.value});
+  }
+
+  handleOnChangeIdentifierNumberOfSupplier(event) {
+    this.setState({identifierNumberOfSupplier: event.target.value});
+  }
+
+  handleOnChangeNameOfSupplier(event) {
+    this.setState({nameOfSupplier: event.target.value});
+  }
+
+  handleOnChangeVatIdentifierNumberOfSupplier(event) {
+    this.setState({vatIdentifierNumberOfSupplier: event.target.value});
+  }
+
+  handleOnChangeStreetOfSupplier(event) {
+    this.setState({streetOfSupplier: event.target.value});
+  }
+
+  handleOnChangeCityOfSupplier(event)  {
+    this.setState({cityOfSupplier: event.target.value});
+  }
+
+  handleOnChangeZipCodeOfSupplier(event) {
+    this.setState({zipCodeOfSupplier: event.target.value});
+  }
+  
+  handleOnChangeIdentifierNumberOfClient(event) {
+    this.setState({identifierNumberOfClient: event.target.value});
+  }
+
+  handleOnChangeNameOfClient(event) {
+    this.setState({nameOfClient: event.target.value});
+  }
+
+  handleOnChangeVatIdentifierNumberOfClient(event) {
+    this.setState({vatIdentifierNumberOfClient: event.target.value});
+  }
+
+  handleOnChangeStreetOfClient(event) {
+    this.setState({streetOfClient: event.target.value});
+  }
+
+  handleOnChangeCityOfClient(event)  {
+    this.setState({cityOfClient: event.target.value});
+  }
+
+  handleOnChangeZipCodeOfClient(event) {
+    this.setState({zipCodeOfClient: event.target.value});
+  }
 
   render() {
     return (
       <form onSubmit={this.handleOnSubmit}>
-        {/* <div> -- ADD LATER
-                    Druh faktury:
-                    <select value={this.state.value} onChange={this.handleChange}>
-                        <option value="invoiceWithoutVat">Faktura bez DPH (nejsem platce DPH)</option>
-                        <option value="invoiceWithVat">Faktura s DPH (jsem platce DPH) - Danovy doklad</option>
-                    </select>
-                </div> */}
 
         <h2>Vystavení nové faktury</h2>
 
@@ -93,140 +160,37 @@ export class Form extends Component {
           <NumberInput label={"Číslo"} value={this.state.invoiceNumber} handler={this.handleOnChangeInvoiceNumber}/>
           <NumberInput label={"Evidenční číslo"} value={this.state.registrationNumber} handler={this.handleOnChangeRegistrationNumber}/>
           <NumberInput label={"Variabilní symbol"} value={this.state.variableSymbol} handler={this.handleOnChangeVariableSymbol}/>
-          <NumberInput label={"Konstantní symbol"} value={this.state.constantSymbol} handler={this.handleOnChangeVariableSymbol}/>
+          <NumberInput label={"Konstantní symbol"} value={this.state.constantSymbol} handler={this.handleOnChangeConstantSymbol}/>
           <StringInput label={"Vystavil"} value={this.state.createdBy} handler={this.handleOnChangeCreatedBy} />
           <DateInput label={"Datum vystavení"} value={this.state.issueDate} handler={this.handleOnChangeIssueDate} />
-          
-
-          <div className="content-field">
-            <div className="content-field__label">Splatnost:</div>
-            <div className="content-field__value">
-              <input type={"date"} value={this.state.dueDate}/>
-            </div>
-          </div>
-
-          <div className="content-field">
-            <div className="content-field__label">Cislo uctu:</div>
-            <div className="content-field__value">
-              <input type={"number"} value={this.state.accountNumber} />
-            </div>
-          </div>
+          <DateInput label={"Splatnost"} value={this.state.dueDate} handler={this.handleOnChangeDueDate} />
+          <NumberInput label={"Cislo uctu"} value={this.state.accountNumber} handler={this.handleOnChangeAccountNumber} />
         </Section>
 
-        <div className="section">
-          <div className="section__header">
-            <h3>Dodavatel:</h3>
-          </div>
-          <div className="section__content">
-            <div className="content-field">
-              <div className="content-field__label">IČO:</div>
-              <div className="content-field__value">
-                <input type={"number"} value={this.state.identifierNumberOfSupplier}/>
-              </div>
-            </div>
+        <Section header={"Dodavatel"}>
+          <NumberInput label={"IČO"} value={this.state.identifierNumberOfSupplier} handler={this.handleOnChangeIdentifierNumberOfSupplier} />
+          <StringInput label={"Jmeno"} value={this.state.nameOfSupplier} handler={this.handleOnChangeNameOfSupplier} />
+          <StringInput label={"DIČ"} value={this.state.vatIdentifierNumberOfSupplier} handler={this.handleOnChangeVatIdentifierNumberOfSupplier} />
+          <StringInput label={"Ulice"} value={this.state.streetOfSupplier} handler={this.handleOnChangeStreetOfSupplier} />
+          <StringInput label={"Město"} value={this.state.cityOfSupplier} handler={this.handleOnChangeCityOfSupplier} />
+          <StringInput label={"PSČ"} value={this.state.zipCodeOfSupplier}  handler={this.handleOnChangeZipCodeOfSupplier} />
+        </Section>
 
-            <div className="content-field">
-              <div className="content-field__label">Jméno:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.nameOfSupplier}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">DIČ:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.vatIdentifierNumberOfSupplier}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">Ulice:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.streetOfSupplier}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">Město:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.cityOfSupplier}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">PSČ:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.zipCodeOfSupplier}/>
-              </div>
-            </div>
-          </div>
-
-          <hr />
-        </div>
-
-        <div className="section">
-          <div className="section__header">
-            <h3>Odběratel:</h3>
-          </div>
-          <div className="section__content">
-            <div className="content-field">
-              <div className="content-field__label">IČO:</div>
-              <div className="content-field__value">
-                <input type={"number"} value={this.state.identifierNumberOfClient}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">Jméno:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.nameOfClient}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">DIČ:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.vatIdentifierNumberOfClient}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">Ulice:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.streetOfClient}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">Město:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.cityOfClient}/>
-              </div>
-            </div>
-
-            <div className="content-field">
-              <div className="content-field__label">PSČ:</div>
-              <div className="content-field__value">
-                <input type={"text"} value={this.state.zipCodeOfClient}/>
-              </div>
-            </div>
-          </div>
-
-          <hr />
-        </div>
+        <Section header={"Odběratel"}>
+        <NumberInput label={"IČO"} value={this.state.identifierNumberOfClient} handler={this.handleOnChangeIdentifierNumberOfClient} />
+          <StringInput label={"Jmeno"} value={this.state.nameOfClient} handler={this.handleOnChangeNameOfClient} />
+          <StringInput label={"DIČ"} value={this.state.vatIdentifierNumberOfClient} handler={this.handleOnChangeVatIdentifierNumberOfClient} />
+          <StringInput label={"Ulice"} value={this.state.streetOfClient} handler={this.handleOnChangeStreetOfClient} />
+          <StringInput label={"Město"} value={this.state.cityOfClient} handler={this.handleOfChangeCityOfClient} />
+          <StringInput label={"PSČ"} value={this.state.zipCodeOfClient}  handler={this.handleOnChangeZipCodeOfClient} />
+        </Section>
 
         <Section header={"Položky faktury"} />
 
-        <div className="section">
-          <div className="section__header">
-            <h3>Poznámka:</h3>
-          </div>
-          <div className="section__content">
+        <Section header={"Poznámka"}>
             <input type={"text"} />
-          </div>
+        </Section>
 
-          <hr />
-        </div>
       </form>
     );
   }
