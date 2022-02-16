25a981347442ba712a52cba754943d7a8aa33087
Maksim Zaytsev <maksim@zaytsev.cz>
2022-02-16 22:11:29 +0100

    fix structure in number input

--- client/src/components/inputs/NumberInput.js
+++ client/src/components/inputs/NumberInput.js
@@ -3,17 +3,15 @@ import { Component } from "react";
 export class NumberInput extends Component {
   render() {
     return (
-      <div className="section__content">
-        <div className="content-field">
-          <div className="content-field__label">{this.props.label}:</div>
-          <div className="content-field__value">
-            <input
-              type="number"
-              value={this.props.value || ""}
-              onChange={this.props.handler}
-              name={this.props.name}
-            />
-          </div>
+      <div className="content-field">
+        <div className="content-field__label">{this.props.label}:</div>
+        <div className="content-field__value">
+          <input
+            type="number"
+            value={this.props.value || ""}
+            onChange={this.props.handler}
+            name={this.props.name}
+          />
         </div>
       </div>
     );
