ef619029cb7f9726a49cca166c4af9bf31afad33
Maksim Zaytsev <maksim@zaytsev.cz>
2022-02-23 21:01:05 +0100

    add basic router to app

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,15 +1,31 @@
 import "./App.scss";
 import Form from "../form/Form.js";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1>Dejfakturu.cz</h1>
-      </header>
-      <Form />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/create-invoice">Create invoice</Link>
+          </li>
+        </ul>
+
+        <Routes>
+          <Route path="/create-invoice" element={<Form />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Homepage</h2>;
 }
 
 export default App;
