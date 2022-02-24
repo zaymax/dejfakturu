87fc4d511432c944c7fdf0e35a930e5cb576c851
Maksim Zaytsev <maksim@zaytsev.cz>
2022-02-24 23:16:31 +0100

    bump - change text on link in menu

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -8,7 +8,7 @@ function App() {
       <div>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">Homepage</Link>
           </li>
           <li>
             <Link to="/create-invoice">Create invoice</Link>
